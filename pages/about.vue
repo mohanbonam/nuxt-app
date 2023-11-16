2ff38755cdd1f3676547ae537f057484addb50de
Mohan Bonam <Mohan.Bonam@ibm.com>
2023-11-16 12:56:53 -0500

    server routes

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,11 @@
+<script setup>
+const { data } = useFetch("/api/routes?name=mario", {
+  method: "post",
+  body: { age: 28 },
+});
+</script>
 <template>
   <section>
-    <p>About page</p>
+    <p>{{ data }}</p>
   </section>
 </template>
